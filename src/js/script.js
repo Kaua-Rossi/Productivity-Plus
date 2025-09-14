5e0b51a385cf34d722655d0da8754dcac0d4ab9f
K_Rossi <kauarossipersonal@gmail.com>
2025-09-14 09:37:36 -0300

    feat: this commit add a basic timer feature
    first feature of the website, a timer section with some controllers that: set the time; start/pause the timer; reset the time
    
    BREAKING CHANGE: closes Basic timer #2

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,73 @@
-// Initial Script
-console.log("Website Loaded :)");
+let timerRunning = false;
+
+function setTimerPopup() {
+    document.querySelector("#timer-set-popup").classList.remove("hidden");
+    document.querySelector("#timer-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        document.querySelector("#timer-set-popup").classList.add("hidden");
+        setTimer(document.querySelector("#hours").value, document.querySelector("#minutes").value, document.querySelector("#seconds").value);
+    });
+    return;
+}
+
+function setTimer(hours = 0, minutes = 0, seconds = 0) {
+    try {
+        if (isNaN(parseInt(hours))) throw "Invalid hours";
+        if (isNaN(parseInt(minutes))) throw "Invalid minutes";
+        if (isNaN(parseInt(seconds))) throw "Invalid seconds";
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
+
+    let timerDisplay = document.querySelector("#timer-display");
+    timerDisplay.textContent = String(hours).padStart(2, "0") + ":" + String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0");
+    console.log(`Timer set to ${timerDisplay.textContent}`);
+    return true;
+}
+
+function timerSetStatus(bool) {
+    if (typeof bool !== "boolean") {
+        console.error("Invalid argument: not a boolean");
+        return false;
+    }
+    timerRunning = bool;
+    console.log(`Timer running status set to ${timerRunning}`);
+    return true;
+}
+
+function timerTick() {
+    if (!timerRunning) return;
+
+    let timerDisplay = document.querySelector("#timer-display");
+    let hours, minutes, seconds;
+    hours = parseInt(timerDisplay.textContent.split(":")[0]);
+    minutes = parseInt(timerDisplay.textContent.split(":")[1]);
+    seconds = parseInt(timerDisplay.textContent.split(":")[2]);
+
+    if (seconds > 0) {
+        seconds--;
+    } else if (minutes > 0) {
+        minutes--;
+        seconds = 59;
+    } else if (hours > 0) {
+        hours--;
+        minutes = 59;
+        seconds = 59;
+    } else {
+        // Timer finished
+        console.log("Timer finished");
+        timerSetStatus(false);
+        return;
+    }
+    timerDisplay.textContent = String(hours).padStart(2, "0") + ":" + String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0");
+}
+
+setInterval(timerTick, 1000);
+
+function timerReset() {
+    setTimer(0, 0, 0);
+    timerSetStatus(false);
+    console.log("Timer reset");
+    return true;
+}
